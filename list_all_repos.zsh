a4257498eec47a790b55b65d245ca7373c081c8a
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-04 19:28:07 -0400

    List all repos

---
+++ list_all_repos.zsh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+# Usage: ./list_all_repos.zsh ORGANIZATION_NAME personal_access_token
+# Get a token here: https://github.com/settings/applications
+
+
+ORG="$1"
+token="$2"
+
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
+
+function query_api {
+  local url_path="$1"
+
+  curl \
+    -u "${token}:x-oauth-basic" \
+    -H 'Accept: application/vnd.github.v3+json' \
+    -H "User-Agent: ${USER_AGENT}" \
+    "https://api.github.com/${url_path}" \
+    2> /dev/null
+}
+
+function all_public_repos_in_organization {
+  query_api "orgs/${ORG}/repos?type=all" | jq --raw-output '.[].name'
+}
+
+all_public_repos_in_organization
