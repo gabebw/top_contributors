41e48ab93ac25d8f01449a288cc030c338e3f9cd
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-04 19:48:15 -0400

    Add main

---
+++ main.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Usage: ./main.zsh ORGANIZATION_NAME TOKEN
+
+ORGANIZATION="$1"
+TOKEN="$2"
+
+GIT_NAME=$(git config user.name)
+
+for repo_name in $(./list_all_repos.zsh "$ORGANIZATION" "$TOKEN")
+do
+  if git clone "https://github.com/${ORGANIZATION}/${repo_name}.git"
+  then
+    cd "$repo_name" &> /dev/null
+    git shortlog -ns | nl | ag "${GIT_NAME}$" | awk '{$1=""; print $0 }' | head -1
+  fi
+
+done
