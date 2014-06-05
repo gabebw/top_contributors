1e9ed8d274977df24a274408975b6b9a1d0b5dc3
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-04 20:08:40 -0400

    Output rank for each repo

--- main.zsh
+++ main.zsh
@@ -6,14 +6,24 @@ ORGANIZATION="$1"
 TOKEN="$2"
 
 GIT_NAME=$(git config user.name)
+TOPLEVEL_DIR=$(pwd)
 
 for repo_name in $(./list_all_public_repos.zsh "$ORGANIZATION" "$TOKEN")
 do
-  echo $repo_name
-  if git clone "https://github.com/${ORGANIZATION}/${repo_name}.git"
-  then
-    cd "$repo_name" &> /dev/null
-    git shortlog -ns | nl | ag "${GIT_NAME}$" | awk '{$1=""; print $0 }' | head -1
+  cd "$TOPLEVEL_DIR"
+  if [[ -d "$repo_name" ]]; then
+    cd "$repo_name"
+    git checkout master &>/dev/null
+    git pull origin master &>/dev/null
+  else
+    git clone "https://github.com/${ORGANIZATION}/${repo_name}.git"
   fi
 
+  cd "$repo_name" &> /dev/null
+  rank=$(git shortlog -ns | nl | ag "^\W+[0-9]+\W+[0-9]+\t${GIT_NAME}$" | cut -f1 | xargs echo)
+
+  if [[ -n "$rank" ]]
+  then
+    echo "$repo_name: #${rank}"
+  fi
 done
